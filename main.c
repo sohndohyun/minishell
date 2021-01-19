9ddc145abd81e055a0daa3bfcdf698e6d04317c9
dsohn <sohndohyun@sondohyeon-ui-Macmini.local>
2021-01-19 20:13:55 +0900

    ascii art added, problem in gnl

--- main.c
+++ main.c
@@ -6,15 +6,29 @@
 /*   By: dsohn <dsohn@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 16:44:15 by dsohn             #+#    #+#             */
-/*   Updated: 2021/01/18 17:46:47 by hyeonski         ###   ########.fr       */
+/*   Updated: 2021/01/19 20:13:24 by dsohn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+#include <stdio.h>
 
 int main(void)
 {
-	write(1, "Hello World!\n", 13);
-	read(1, "Hello", 1);
-	return (0);
+	int fd;
+	char *line;
+
+	line = 0;
+	if (!(fd = open("ascii_art.txt", O_RDONLY)))
+	{
+		write(1, "\nError in open\n", 15);
+		return (0);
+	}
+	while (get_next_line(fd, &line) != 0)
+	{
+		printf("%s\n", line);
+		free (line);
+	}
 }
